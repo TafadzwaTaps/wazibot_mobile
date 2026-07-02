a6f6ec0ba1c2b4fe4ce741337f8dbf718b3a04ac
TafadzwaTaps <w65242@student.wsiz.edu.pl>
2026-07-02 20:05:42 +0200

    fix: wazibot flutter application v2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,13 +18,15 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
         isCoreLibraryDesugaringEnabled = true
     }
 
-    kotlinOptions {
-        jvmTarget = "1.8"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+        }
     }
 
     buildTypes {
